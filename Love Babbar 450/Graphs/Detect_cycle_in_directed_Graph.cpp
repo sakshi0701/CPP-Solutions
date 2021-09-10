2b4a3256788d934f79d3952f359989e04f54ed5f
Sakshi Dwivedi <60813465+sakshi0701@users.noreply.github.com>
2021-09-11 01:31:29 +0530

    Update Detect_cycle_in_directed_Graph.cpp

--- Love Babbar 450/Graphs/Detect_cycle_in_directed_Graph.cpp
+++ Love Babbar 450/Graphs/Detect_cycle_in_directed_Graph.cpp
@@ -4,6 +4,7 @@ using namespace std;
 class Solution {
   public:
   
+//   Using DFS:
     bool solve(int src, vector<int> &vis, vector<int> &order, vector<int> g[])
     {
         vis[src] = 1;
@@ -37,6 +38,46 @@ class Solution {
         }
         return false;
     }
+  
+  // Using BFS:
+  // If indegree is greater than 0 for all the nodes at any point means there's a cycle
+  bool isCyclicBFS(int V, vector<int> adj[]) {
+    queue<int> q;
+    vector<int> indegree(V, 0);
+    // calculating indegree:
+    for(int i=0; i<V; i++)
+    {
+        for(auto x : adj[i]) indegree[x]++;
+    }
+    
+    for(int i=0; i<V; i++)
+    {
+        if(indegree[i] == 0) q.push(i);
+    }
+    
+    int counter = q.size();
+    while(!q.empty())
+    {
+        int a = q.front();
+        q.pop();
+        // (works similar to topological sort: )
+        for(auto x : adj[a])
+        {
+            indegree[x]--;
+            if(indegree[x] == 0)
+            {
+                q.push(x);
+                counter++;
+            }
+        }
+    }
+    
+    // checking how many nodes have indegree 0 with counnter: (if all then no cycle)
+    if(counter == V) return false;
+    
+    return true;
+    
+    }
 };
 
 /*
@@ -60,6 +101,7 @@ int main() {
 
         Solution obj;
         cout << obj.isCyclic(V, adj) << "\n";
+        cout << obj.isCyclicBFS(V, adj) << "\n";
     }
 
     return 0;
